6ff83b0be9ea549fdca80caf0b60c95841da8825
Dmytro-Ivanenko <dmitro0399@gmail.com>
2022-10-30 18:05:24 +0200

    task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,16 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+debugger;
+const ingredientsList = document.querySelector("[id = ingredients]");
+
+const addIngredientsElem = arr => {
+	const elemArr = arr.map(ingredient => {
+		const elem = document.createElement("li");
+		elem.textContent = ingredient;
+
+		return elem;
+	});
+
+	ingredientsList.append(...elemArr);
+};
+
+addIngredientsElem(ingredients);
