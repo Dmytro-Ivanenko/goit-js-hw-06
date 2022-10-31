013e201214e3a50969fac121f6e9fbcbd9d0c9dc
Dmytro-Ivanenko <dmitro0399@gmail.com>
2022-10-31 22:33:32 +0200

    task 7

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,1 +1,7 @@
 "use strict";
+
+const rangeHandler = event => {
+	document.querySelector("#text").style.fontSize = `${event.currentTarget.value}px`;
+};
+
+document.querySelector("#font-size-control").addEventListener("input", rangeHandler);
