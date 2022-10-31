a7b73ee620fd1d465e104891b5c5a35c1848006f
Dmytro-Ivanenko <dmitro0399@gmail.com>
2022-10-31 23:46:26 +0200

    task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,7 @@
 function getRandomHexColor() {
 	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorHandler = () => (document.body.style.backgroundColor = getRandomHexColor());
+
+document.querySelector(".change-color").addEventListener("click", changeColorHandler);
