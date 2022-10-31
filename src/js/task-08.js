5b9ec57363fdb629d6576a4be99ed4c636609385
Dmytro-Ivanenko <dmitro0399@gmail.com>
2022-10-31 23:29:46 +0200

    task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const submitHandler = event => {
+	event.preventDefault();
+
+	const userData = {
+		email: event.currentTarget.elements.email.value,
+
+		password: event.currentTarget.elements.password.value,
+	};
+
+	if (userData.email === "" || userData.password === "") {
+		alert("Please fill in all the fields!");
+		return;
+	}
+
+	console.log(userData);
+
+	event.currentTarget.reset();
+};
+
+document.querySelector(".login-form").addEventListener("submit", submitHandler);
