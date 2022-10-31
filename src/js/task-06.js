a24007def09da9fabb7279e1c98d1624d83f3763
Dmytro-Ivanenko <dmitro0399@gmail.com>
2022-10-31 21:59:40 +0200

    task 6

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,1 +1,9 @@
 "use strict";
+
+const validationInputHandler = event => {
+	const input = event.currentTarget;
+
+	input.value.length === 6 ? input.setAttribute("class", "valid") : input.setAttribute("class", "invalid");
+};
+
+document.querySelector("#validation-input").addEventListener("blur", validationInputHandler);
