70c882f5df71d043d10a326f93856cae5c97851f
Dmytro-Ivanenko <dmitro0399@gmail.com>
2022-10-31 21:20:34 +0200

    task 5

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const output = document.querySelector("#name-output");
+const input = document.querySelector("#name-input");
+
+const inputHandler = event => {
+	event.currentTarget.value === ""
+		? (output.textContent = "Anonymous")
+		: (output.textContent = event.currentTarget.value);
+};
+
+input.addEventListener("input", inputHandler);
